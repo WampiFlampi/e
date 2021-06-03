d685da5f51f2a5294d82c78becc13d28083ec216
WampiFlampi <thanson75@mtlstudents.net>
2021-06-03 03:10:52 -0400

    e

--- higher-order.hs
+++ higher-order.hs
@@ -7,10 +7,29 @@ resolve e ls | e == m    = 0
                where
                  m = ls!!(div (length ls) 2)
 
-f z x y = x^2+y^2 == z^2
+f1 z x y = x^2+y^2 == z^2
 
-pyth n = [(x,y,z) | z <- [1..n],x <- [1..z-1],y <- [x..z-1] ,f z x y]
+pyth n = [(x,y,z) | z <- [1..n],x <- [1..z-1],y <- [x..z-1] ,f1 z x y]
                                                              
 c a b = a*b
 
 f0 = c 10
+
+
+
+length1 :: Foldable t => t a -> Int
+
+length1 = foldr (\x n -> 1+n) 0
+
+-- r = foldr (\x xs -> xs++[x] ) []
+
+-- f = foldr (\x n -> (x^2):n) []
+
+
+f p x xs | p x       = x : xs
+         | otherwise = xs
+
+filter1 p = foldr c []
+     where
+        c = f p
+
